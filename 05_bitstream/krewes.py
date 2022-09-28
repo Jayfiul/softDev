cf7d6776d0ff7a11b701f6187e3e569b705d1324
Jayfiul <azizyusha@gmail.com>
2022-09-28 19:57:57 -0400

    added 05_bitstream work

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,17 @@
+import random as r
+
+with open('krewes.txt') as f:
+    krewes = f.readline()
+
+classes = {2:[],7:[],8:[]}
+
+def get_devo(krewes):
+    person = krewes.split('@@@')
+    for i in person:
+        pinfo = i.split('$$$')
+        classes[int(pinfo[0])].append([pinfo[1],pinfo[2]])
+    pd = r.choice(list(classes.keys()))
+    person = r.choice(classes[pd])
+    return(f"{pd} : {person[0]}, ducky named: {person[1]}")
+
+print(get_devo(krewes))
