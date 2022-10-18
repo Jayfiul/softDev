76804c4464cd7334b66ff776a6502e64539c15da
Jayfiul <azizyusha@gmail.com>
2022-10-18 19:06:22 -0400

    copied from groupmates repo

--- 12_flask-forms/app.py
+++ 12_flask-forms/app.py
@@ -1,37 +1,14 @@
-# Yusha, Jeff, Fang
+# Bottlers (Jeff Chen, Yusha Aziz, Fang Chen)
 # SoftDev
-# K12 -- Take and Give
-# 2022-10-17
-# time spent: 45 mins
+# K12 Take and Give (Flask GET and POST)
+# October 17 2022
+# time spent: 0.5 hrs
 
-
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
-
-#the conventional way:
-#from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
 
-
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
-Some will work as written; other sections will not. 
-TASK: Predict which...
-Devise some simple tests you can run to "take apart this engine," as it were.
-Execute your tests.
-Process results.
-
-PROTIP: Insert your own in-line comments
- wherever they will help
-  your future self and/or current teammates
-   understand what is going on.
-'''
-
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -47,7 +24,7 @@ def disp_loginpage():
     return render_template( 'login.html' )
 
 
-@app.route("/auth", methods = ['GET', 'POST']) # , methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -56,19 +33,12 @@ def authenticate():
     print(request)
     print("***DIAG: request.args ***")
     print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print("***DIAG: request.args['username']  ***")
+    print(request.form.get('username'))
     print("***DIAG: request.headers ***")
     print(request.headers)
-    
-    if request.method == 'POST': #if the request method is POST, which can be determined by looking at the python code
-        username = request.form.get('username')
-        return render_template('response.html', username=username, method = "POST")  #renders the template with the username, response to a form submission
-    elif request.method == 'GET': #if the request is GET, it will use the GET request version
-        username = request.args['username']
-        return render_template('response.html', username=username, method = "GET")
-    else:
-        return render_template('response.html', method = "ERROR")
+    return render_template( 'response.html', username=request.form.get('username'))  #response to a form submission
+
 
     
 if __name__ == "__main__": #false if this file imported as module
--- 12_flask-forms/readme.md
+++ 12_flask-forms/readme.md
@@ -1,6 +1,8 @@
-## The Difference Bettwen GET and POST
-- GET is when the python code recieves/gets information from HTML
-  - When you use GET the request code is differnt: `request.args['variable_name_in_HTML']`
-- POST is when the HTML code sends/posts information to the python code to use 
-  - When you use the POST method to recieve information the request code is different: `request.form.get('variable_name_in_HTML')` 
-- The specific line in the code that determines whether the GET method or POST method is being used is in **line 37** of *login.html*. If the `method = ""` is equal to GET it will use the GET way of displaying information from the USER, and if it is POST, it will use the POST way of displaying the information that the user puts in the local host. 
+# Bottlers: Fang, Jeff, Yusha  
+SoftDev, pd 2  
+K12 - Flask Forms  
+17 Oct 2022  
+
+Difference between GET and POST: (?)  
+When we use a GET method, the python script takes the information from the html page
+When we use a POST method, the html page sends information to the python script
