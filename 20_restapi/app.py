bbba85c920672f5e7ab9a104f44c6db5e3780b01
Jayfiul <azizyusha@gmail.com>
2022-11-21 17:59:26 -0500

    midway

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,12 +1,19 @@
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+import json
+import requests
+
 
 app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
 
-@app.route("/") # Q1: What points of reference do you have for meaning of '/'?
+f = open("key_nasa.txt", "r")
+keyNasa = f.read() 
+
+print(keyNasa)
+
+@app.route("/") 
 def hello_world():
-    print(__name__) # Q2: Where will this print to? Q3: What will it print?
     return render_template('main.html', key = keyNasa) 
 
 app.run()  # Q5: Where have you seen similar constructs in other languages?
