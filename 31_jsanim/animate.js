efc25ee7f80580a327fd5680e37c9c5a97558f56
Jayfiul <azizyusha@gmail.com>
2023-04-26 08:47:30 -0400

    forgot to push from last night

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -2,55 +2,54 @@ var c = document.getElementById("playground");
 var dotButton = document.getElementById("buttonCircle");
 var stopButton = document.getElementById("buttonStop");
 
-var ctx = canvas.getContext("2d");
+var ctx = c.getContext("2d");
 
-ctx.fillStyle = "#0000ff";
+ctx.fillStyle = "#0000FF";
 
 var requestID;
 
 var clear = (e) => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, c.width, c.height);
 };
 
 var radius = 0;
 var growing  = true;
 
 
-let drawCirc = function(e) {
-    let x = e.offsetX;
-    let y = e.offsetY;
-    ctx.fillStyle = "#0000ff";
+let drawCirc = function() {
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.arc(c.width/2, c.height/2, radius, 0, Math.PI*2);
     ctx.fill();
+    ctx.closePath();
   }
 
 var drawDot = () => {
-    clear;
-    window.requestAnimationFrame();
-    drawCirc(e);
-    if (growing == true){
+    clear();
+    if(radius==c.width/2){
+        growing = false;
+    }
+    else if(radius==0){
+        growing = true;
+    }
+
+    if (growing){
         radius++;
     }else{
         radius--;
-    }
-    if(radius==1000){
-        growing == false;
-        radius--;
-    }
-    if(radius==0){
-        growing == true;
-        radius++;
-    }
-window.cancelAnimationFrame();
-};
+     }
+     
+    drawCirc();
+
+    window.cancelAnimationFrame(requestID);
+    requestID = window.requestAnimationFrame(drawDot);
+}
 
 
 var stopIt = () => {
     console.log("stopIt invoked...")
     console.log(requestID);
-    window.cancelAnimationFrame();
-};
+    window.cancelAnimationFrame(requestID);
+}
 
-dotButton.addEventListener("click",drawDot)
-stopButton.addEventListener("click", stopIt)
+dotButton.addEventListener("click",drawDot);
+stopButton.addEventListener("click", stopIt);
